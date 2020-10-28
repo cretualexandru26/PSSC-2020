4ecb007b750e0f2fc9b65c18098e8273f8be8c75
Cretu Alexandru <alexandru.cretu26@yahoo.com>
2020-10-28 21:38:45 +0200

    Tema 5

---
+++ Cretu Dumitru-Alexandru/L05/StackOverflow/StackOverflow.Domain.Schema/Questions/AddQuestionOp/AddQuestionCmd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StackOverflow.Domain.Schema.Questions.AddQuestionOp
+{
+    public class AddQuestionCmd
+    {
+        public AddQuestionCmd( string title, QuestionLength body, QuestionTags tags)
+        {
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+
+        [Required]
+        public string Title { get; }
+        [Required]
+        [MaxLength(1000)]
+        public QuestionLength Body { get; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(3)]
+        public QuestionTags Tags { get; }
+    }
+}
